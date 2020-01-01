e77ac465e1f1145097cec003319285164c7bd212
k-law21 <kwame@udel.edu>
2020-01-01 12:01:54 -0500

    Method Signatures

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -3,5 +3,9 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class MainApplication {
+public class  MainApplication {
+
+public static void main(String args[]){
+
+}
 }
--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,14 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+        public static void add (Integer info){ }
+
+        public static void remove(Integer info){}
+
+        public  static void find(Integer number){}
+
+        public Boolean contains(Integer num){ return null; }
+
+
 }
